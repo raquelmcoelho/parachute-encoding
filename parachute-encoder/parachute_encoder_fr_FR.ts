1734c7324955828b8ef4834730c770013805b003
Raquel Sousa <raquelmcoelhodesousa@gmail.com>
2025-03-25 17:09:48 +0100

    feat: internationalization

--- parachute-encoder/parachute_encoder_fr_FR.ts
+++ parachute-encoder/parachute_encoder_fr_FR.ts
@@ -1,3 +1,17 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="fr_FR"></TS>
+<TS version="2.1" language="fr_FR">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>Fenêtre Principale</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="11"/>
+        <source>Bonjour le monde!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
--- parachute-encoder/parachute_encoder_pt_BR.ts
+++ parachute-encoder/parachute_encoder_pt_BR.ts
@@ -1,3 +1,17 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="pt_BR"></TS>
+<TS version="2.1" language="pt_BR">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation type="Janela Principal"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="11"/>
+        <source>Ola, Mundo!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
