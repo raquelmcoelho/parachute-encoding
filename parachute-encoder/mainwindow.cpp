6a932f7c47fcfeeef3d18ef6110395417a0374cb
ViniciusGN <170115437@aluno.unb.br>
2025-03-26 20:06:32 +0100

    Feat: MainWindow edited with new features

--- parachute-encoder/mainwindow.cpp
+++ parachute-encoder/mainwindow.cpp
@@ -1,33 +1,97 @@
-#include <mainwindow.h>
+#include "mainwindow.h"
+#include <QMenuBar>
+#include <QMenu>
+#include <QAction>
+#include <QLabel>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QPushButton>
+#include <QLineEdit>
+#include <QColorDialog>
+#include <QMessageBox>
+#include "view/parachutewidget.h"
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
 {
     setWindowTitle("Parachute Encoder");
-    setGeometry(100, 100, 600, 600);
+    resize(800, 600);
 
     parachute_widget = new ParachuteWidget();
     sector_slider = new QSlider(Qt::Horizontal);
     sector_slider->setMinimum(7);
     sector_slider->setMaximum(35);
     sector_slider->setValue(21);
+    connect(sector_slider, &QSlider::valueChanged, this, &MainWindow::updateSectors);;
+
+    QLabel* trapezoidLabel = new QLabel("Trapezoides");
+    trapezoidLabel->setMinimumSize(300, 300);
+    trapezoidLabel->setFrameShape(QFrame::Box);
+    trapezoidLabel->setAlignment(Qt::AlignCenter);
+
+    QHBoxLayout* imageLayout = new QHBoxLayout;
+    imageLayout->addWidget(parachute_widget);
+    imageLayout->addWidget(trapezoidLabel);
+
+    // Sliders
+    sector_slider = new QSlider(Qt::Horizontal);
+    sector_slider->setRange(7, 35);
+    sector_slider->setValue(21);
     connect(sector_slider, &QSlider::valueChanged, this, &MainWindow::updateSectors);
 
     track_slider = new QSlider(Qt::Horizontal);
-    track_slider->setMinimum(2);
-    track_slider->setMaximum(10);
+    track_slider->setRange(2, 10);
     track_slider->setValue(5);
     connect(track_slider, &QSlider::valueChanged, this, &MainWindow::updateTracks);
 
-    QVBoxLayout *layout = new QVBoxLayout;
+    // Controles inferiores
+    QLineEdit* messageInput = new QLineEdit;
+    messageInput->setPlaceholderText("Insira a mensagem binária (ex: 010101)");
+
+    QPushButton* colorButton = new QPushButton("Escolher Cor do Bit 1");
+    QPushButton* encodeButton = new QPushButton("Codificar");
+
+    connect(colorButton, &QPushButton::clicked, this, [this]() {
+        QColor chosen = QColorDialog::getColor(Qt::red, this, "Escolha a cor para o bit 1");
+        if (chosen.isValid()) {
+            bitOneColor = chosen;
+        }
+    });
+
+    connect(encodeButton, &QPushButton::clicked, this, [messageInput]() {
+        QString message = messageInput->text();
+        // Aqui você pode validar e enviar para o Presenter/Model
+        QMessageBox::information(nullptr, "Mensagem", "Mensagem binária: " + message);
+    });
+
+    QHBoxLayout* controlsLayout = new QHBoxLayout;
+    controlsLayout->addWidget(messageInput);
+    controlsLayout->addWidget(colorButton);
+    controlsLayout->addWidget(encodeButton);
+
+    // Layout final
+    QVBoxLayout* layout = new QVBoxLayout;
+    layout->addLayout(imageLayout);
     layout->addWidget(new QLabel("Setores"));
     layout->addWidget(sector_slider);
     layout->addWidget(new QLabel("Pistas"));
     layout->addWidget(track_slider);
-    layout->addWidget(parachute_widget);
+    layout->addLayout(controlsLayout);
 
-    QWidget *container = new QWidget;
+    QWidget* container = new QWidget;
     container->setLayout(layout);
     setCentralWidget(container);
+
+    // Menu
+    QMenu* fileMenu = menuBar()->addMenu("Arquivo");
+    QAction* exitAction = new QAction("Sair", this);
+    connect(exitAction, &QAction::triggered, this, &QMainWindow::close);
+    fileMenu->addAction(exitAction);
+
+    QMenu* helpMenu = menuBar()->addMenu("Ajuda");
+    QAction* aboutAction = new QAction("Sobre", this);
+    connect(aboutAction, &QAction::triggered, this, []() {
+        QMessageBox::about(nullptr, "Sobre", "Parachute Encoder v1.0\nDesenvolvido por Vinicius.");
+    });
 }
 
 void MainWindow::updateSectors(int value)
@@ -41,3 +105,4 @@ void MainWindow::updateTracks(int value)
     parachute_widget->tracks = value;
     parachute_widget->update();
 }
+
--- parachute-encoder/mainwindow.h
+++ parachute-encoder/mainwindow.h
@@ -2,39 +2,27 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include <QApplication>
-#include <QMainWindow>
-#include <QWidget>
-#include <QPainter>
-#include <QPolygonF>
-#include <QVBoxLayout>
 #include <QSlider>
-#include <QLabel>
-#include <cmath>
-#include <QString>
-#include <view/parachutewidget.h>
+#include <QColor>
 
-QT_BEGIN_NAMESPACE
-namespace Ui {
-class MainWindow;
-}
-QT_END_NAMESPACE
-
+class ParachuteWidget; // Se você usar um personalizado
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
-private:
-    ParachuteWidget *parachute_widget;
-    QSlider *sector_slider;
-    QSlider *track_slider;
-
 public:
     MainWindow(QWidget *parent = nullptr);
-public slots:
-    void updateSectors(int value);
 
+private slots:
+    void updateSectors(int value);
     void updateTracks(int value);
+
+private:
+    ParachuteWidget* parachute_widget;
+    QSlider* sector_slider;
+    QSlider* track_slider;
+    QColor bitOneColor = Qt::red;
 };
+
 #endif // MAINWINDOW_H
